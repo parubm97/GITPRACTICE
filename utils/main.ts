cc732536d2368f4eb8bdd3a6d28ff166d6c74620
Parvathy <parubm97@gmail.com>
2026-07-27 11:07:41 +0530

    commited main file

--- utils/main.ts
+++ utils/main.ts
@@ -1,3 +1,4 @@
 console.log("print power of a number")
 console.log("multiply function is working");
 console.log("multiply function is working practixcing");
+console.log("multiply function is working ");
