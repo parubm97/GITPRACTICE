e40f14dcc12af45591f9c5f492350de3d0a3accf
Parvathy <parubm97@gmail.com>
2026-07-27 11:49:45 +0530

    main.ts is updated with branch rule

--- utils/main.ts
+++ utils/main.ts
@@ -3,3 +3,4 @@ console.log("multiply function is working");
 console.log("multiply function is working practixcing");
 console.log("multiply function is working ");
 console.log("fetching config file");
+console.log("branch rule setup is working");
