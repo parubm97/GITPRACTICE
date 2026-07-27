b9fdeaba456d8a0b14e2af1a741fd9438180e731
Parvathy <parubm97@gmail.com>
2026-07-27 12:09:40 +0530

    main.ts file has updated

--- utils/main.ts
+++ utils/main.ts
@@ -4,3 +4,4 @@ console.log("multiply function is working practixcing");
 console.log("multiply function is working ");
 console.log("fetching config file");
 console.log("branch rule setup is working");
+console.log("pull request has been created");
