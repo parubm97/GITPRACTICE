0f0a8fa963b4b9147d5025b45ae081c6f32ee2e7
Parvathy <parubm97@gmail.com>
2026-07-27 11:31:14 +0530

    fetching changes in main.ts file

--- utils/main.ts
+++ utils/main.ts
@@ -2,3 +2,4 @@ console.log("print power of a number")
 console.log("multiply function is working");
 console.log("multiply function is working practixcing");
 console.log("multiply function is working ");
+console.log("fetching config file");
